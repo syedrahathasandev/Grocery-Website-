b5e4c7ea9f08af449cbfd3e182faa33676027021
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-16 01:29:29 +0600

    update

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
-
+import FruitsCat from '../../assets/fruits-and-Veggies.png'
+import SeaFoodCat from '../../assets/meat-and-seafood.png'
 const Category = () => {
   return (
    <section>
@@ -11,4 +12,29 @@ const Category = () => {
   )
 }
 
-export default Category
+export default Category 
+
+
+const category = [
+    {
+        id: 1,
+      title: 'Fruits & Veggies',
+      description: 'Fresh, organic produce sourced daily from local farms. Explore a wide range of seasonal fruits and crisp vegetables.',
+      image: FruitsCat
+
+    },
+    {
+        id: 2,
+      title: 'Fruits & Veggies',
+      description: 'Fresh, organic produce sourced daily from local farms. Explore a wide range of seasonal fruits and crisp vegetables.',
+      image: FruitsCat
+
+    },
+    {
+        id: 3,
+      title: 'Meat & SeaFood',
+      description: 'High-quality, responsibly sourced meat and seafood. Choose from fresh cuts, marinated options, and more.',
+      image: SeaFoodCat
+
+    },
+]
