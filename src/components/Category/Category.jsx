9d10bb9c74691472b4ad60df5f9d3130845397db
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-16 01:15:11 +0600

    update

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Category = () => {
+  return (
+   <section>
+    <div className='py-20'>
+        <Heading  highlight="Shop" heading="by Category"/>
+    </div>
+   </section>
+  )
+}
+
+export default Category
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Hero from '../Hero/Hero'
+import Category from '../Category/Category'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Category />
     </div>
   )
 }
