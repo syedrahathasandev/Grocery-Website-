f7c400889541a8221ca17ca323df0121c6a3fb92
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-20 03:15:14 +0600

    lolo

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -5,7 +5,7 @@ import Basket from '../../assets/basket-full-vegetables.png'
 
 const Values = () => {
 
-  const LeftValues = Value.map(item=>{
+  const leftValues = Value.slice(0, 2).map(item=>{
     return(
       <div>
         <div>
@@ -24,25 +24,17 @@ const Values = () => {
       <div className='max-w-[1400px] mx-auto px-10 py-20'>
         <Heading highlight="Our" heading="Value" />
 
-        <div className='flex items-center justify-between gap-10'>
+        <div className='flex '>
 
-          {/* Left side (Values) */}
-          <div className='grid grid-cols-2 gap-8'>
-            {Value.map((item) => (
-              <div key={item.id} className='p-5 shadow-lg rounded-xl'>
-                <div className='text-orange-500 text-2xl mb-3'>
-                  {item.icon}
-                </div>
-                <h3 className='font-bold text-lg mb-2'>{item.title}</h3>
-                <p className='text-sm text-zinc-600'>{item.para}</p>
-              </div>
-            ))}
+          {/* Left Values */}
+          <div>
+              {leftValues}
           </div>
 
-          {/* Right side (Image) */}
           <div>
             <img src={Basket} alt="basket" />
           </div>
+          {/* Right Value */}
 
         </div>
       </div>
@@ -62,7 +54,7 @@ const Value = [
   },
   {
     id: 2,
-    title: 'Always Fresh', // fixed spelling
+    title: 'Always Fresh', 
     para: 'It is a long established fact that a reader will be distracted by the readable.',
     icon: <FaLeaf />
   },
