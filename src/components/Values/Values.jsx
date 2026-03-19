819d2bedffcb97b406c897040c711e6c509254f2
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-20 04:07:21 +0600

    update

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -5,37 +5,53 @@ import Basket from '../../assets/basket-full-vegetables.png'
 
 const Values = () => {
 
-  const leftValues = Value.slice(0, 2).map(item=>{
-    return(
-      <div>
+  const leftValues = Value.slice(0, 2).map(item => {
+    return (
+      <div className='flex flex-row-reverse items-center gap-7'>
         <div>
-          <span>{item.icon}</span>
+          <span className='flex justify-center items-center text-3xl text-white bg-gradient-to-b from-orange-400 to-orange-500 w-15 h-15 rounded-full'>{item.icon}</span>
         </div>
-        <div>
-          <h3>{item.title}</h3>
-          <p>{item.para}</p>
+        <div className='text-right'>
+          <h3 className='text-zinc-800 text-3xl font-bold'>{item.title}</h3>
+          <p className='text-zinc-600 mt-2'>{item.para}</p>
         </div>
       </div>
     )
-  } )
+  })
+
+    const rightValues = Value.slice(2).map(item => {
+    return (
+      <div className='flex items-center gap-7'>
+        <div>
+          <span className='flex justify-center items-center text-3xl text-white bg-gradient-to-b from-orange-400 to-orange-500 w-15 h-15 rounded-full'>{item.icon}</span>
+        </div>
+        <div >
+          <h3 className='text-zinc-800 text-3xl font-bold'>{item.title}</h3>
+          <p className='text-zinc-600 mt-2'>{item.para}</p>
+        </div>
+      </div>
+    )
+  })
 
   return (
     <section>
       <div className='max-w-[1400px] mx-auto px-10 py-20'>
         <Heading highlight="Our" heading="Value" />
 
-        <div className='flex '>
+        <div className='flex md:flex-row flex-col gap-5 mt-15'>
 
           {/* Left Values */}
-          <div>
-              {leftValues}
+          <div className='min-h-100 flex flex-col justify-between' >
+            {leftValues}
           </div>
 
-          <div>
-            <img src={Basket} alt="basket" />
+          <div className='md:flex w-1/2 hidden'>
+            <img src={Basket}/>
           </div>
           {/* Right Value */}
-
+          <div className='min-h-100 flex flex-col justify-between' >
+            {rightValues}
+          </div>
         </div>
       </div>
     </section>
@@ -54,7 +70,7 @@ const Value = [
   },
   {
     id: 2,
-    title: 'Always Fresh', 
+    title: 'Always Fresh',
     para: 'It is a long established fact that a reader will be distracted by the readable.',
     icon: <FaLeaf />
   },
