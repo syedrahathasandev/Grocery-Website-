599101a101d88cea01151f1ea6d2898635c5fd6e
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-23 02:37:32 +0600

    values update

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -7,11 +7,11 @@ const Values = () => {
 
   const leftValues = Value.slice(0, 2).map(item => {
     return (
-      <div className='flex flex-row-reverse items-center gap-7'>
+      <div className='flex md:flex-row-reverse items-center gap-7'>
         <div>
           <span className='flex justify-center items-center text-3xl text-white bg-gradient-to-b from-orange-400 to-orange-500 w-15 h-15 rounded-full'>{item.icon}</span>
         </div>
-        <div className='text-right'>
+        <div className='md:text-right'>
           <h3 className='text-zinc-800 text-3xl font-bold'>{item.title}</h3>
           <p className='text-zinc-600 mt-2'>{item.para}</p>
         </div>
@@ -38,10 +38,10 @@ const Values = () => {
       <div className='max-w-[1400px] mx-auto px-10 py-20'>
         <Heading highlight="Our" heading="Value" />
 
-        <div className='flex md:flex-row flex-col gap-5 mt-15'>
+        <div className='flex md:flex-row flex-col gap-15 md:gap-5 mt-15'>
 
           {/* Left Values */}
-          <div className='min-h-100 flex flex-col justify-between' >
+          <div className='md:min-h-100 gap-15 flex flex-col justify-between' >
             {leftValues}
           </div>
 
@@ -49,7 +49,7 @@ const Values = () => {
             <img src={Basket}/>
           </div>
           {/* Right Value */}
-          <div className='min-h-100 flex flex-col justify-between' >
+          <div className='md:min-h-100 gap-15 flex flex-col justify-between' >
             {rightValues}
           </div>
         </div>
