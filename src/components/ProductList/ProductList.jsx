0dbd25112b6d778af1d8d1e2d25df95e33fe6e86
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-24 01:58:36 +0600

    update

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,4 @@
+import Butter from '../../assets/butter.png'
+import Strawberry from '../../assets/strawberry.png'
+import Eggs from '../../assets/eggs.png'
+import Cabbage from
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heading from '../Heading/Heading'
 
 const Products = () => {
 
-        const Categories = ['All','Fruits','Vegetables','Dairy','SeaFood']
+    const Categories = ['All', 'Fruits', 'Vegetables', 'Dairy', 'SeaFood']
+    const [activeTab, setActiveTab] = useState('All');
 
-  return (
-    <section>
-        <div className='max-w-[1400px] mx-auto px-10 py-20'>
-        <Heading highlight="Our" heading="Products"/>
+    return (
+        <section>
+            <div className='max-w-[1400px] mx-auto px-10 py-20'>
+                <Heading highlight="Our" heading="Products" />
 
-        {/* tabs */}
-        <div className='flex gap-3 justify-center mt-10'>
-            {Categories.map(Category=>{
-                return(
-                    <button key={Category} className='bg-zinc-100 px-5 py-2 text-lg'>
-                        {Category}
-                    </button>
-                )
-            })}
-        </div>
-        </div>
-    </section>
-  )
+                {/* tabs */}
+                <div className='flex gap-3 justify-center mt-10'>
+                    {Categories.map(Category=> {
+                        return (
+                            <button key={Category} 
+                            className={`px-5 py-2 text-lg rounded-lg cursor-pointer
+                            ${activeTab === Category ? 'bg-gradient-to-b from-orange-400 to-orange-500 text-white' : 'bg-zinc-100'}`}
+                            onClick={()=>setActiveTab(Category)}>
+                                {Category}
+                            </button>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Products
