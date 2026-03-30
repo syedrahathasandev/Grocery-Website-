86d4deca05d6f62031a77a30e9dfe439da2ec359
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-31 00:31:23 +0600

    update

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 const Discount = () => {
   return (
-    <div>
-      
+   <section className='bg-zinc-100'>
+    <div className='mx-w-[1400px] mx-auto px-10 py-10'>
+        <span>20%</span>
+        <div>
+            <h3 className='text-7xl text-zinc-800 font-bold'>First Order Discount!</h3>
+            <p className='text-zinc-600 my-6'>Enjoy an exclusive first order discount on our grocery website! Shop fresh essentials and save big on your first purchase. Fast delivery and quality guaranteed.</p>
+            <Button content="Get a Discount" />
+        </div>
     </div>
+   </section>
   )
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero'
 import Category from '../Category/Category'
 import Values from '../Values/Values'
 import Products from '../Products/Products'
+import Discount from '../Discount/Discount'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Category />
       <Values />
       <Products />
+      <Discount />
     </div>
   )
 }
