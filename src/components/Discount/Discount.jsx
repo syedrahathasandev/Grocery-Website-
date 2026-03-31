de57b1e8ed6a9b45d7495f4c68dd5af6bb155f3e
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-03-31 11:43:57 +0600

    update

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -10,7 +10,6 @@ const Discount = () => {
             <h3 className='text-7xl text-zinc-800 font-bold'>First Order Discount!</h3>
             <p className='text-zinc-600 my-6'>Enjoy an exclusive first order discount on our grocery website! Shop fresh essentials and save big on your first purchase. Fast delivery and quality guaranteed.</p>
             <Button content="Get a Discount" />
-            
         </div>
     </div>
    </section>
