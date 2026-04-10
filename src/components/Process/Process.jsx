c2edfe3c47305fcd4d2bd968df69d44909280d2c
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-04-11 00:25:07 +0600

    update

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -73,12 +73,5 @@ const steps = [
     para: 'It is a long established fact that a reader',
     icon: <BsTruck />,
   },
-  {
-    id: 4,
-    number: <TbCircleNumber4Filled />,
-    title: 'Logistics',
-    para: 'It is a long established fact that a reader',
-    icon: <BsTruck />,
-  },
 
 ]
