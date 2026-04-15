e8d18595be75d5696a9d71721e9d31809de8f5e3
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-04-16 02:29:13 +0600

    saying

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -6,6 +6,11 @@ const Testimonials = () => {
    <section>
     <div className='max-w-[1400px] px-10 mx-auto py-20'>
         <Heading highlight="Customers" heading="Saying"/>
+
+        <div className='py-5'>
+
+        </div>
+        
     </div>
    </section>
   )
