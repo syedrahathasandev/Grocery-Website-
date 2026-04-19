344d1b549947cd9883eaa9a544f90ceb089e15ef
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-04-20 02:41:54 +0600

    next

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
 const Testimonials = () => {
   return (
@@ -7,10 +8,17 @@ const Testimonials = () => {
       <div className='max-w-[1400px] px-10 mx-auto py-20'>
         <Heading highlight="Customers" heading="Saying" />
 
-        <div className='py-5'>
+        <div className='flex justify-end gap-x-3  py-5'>
+
+         <button className='text-2xl text-zinc-800 rounded-lg w-11 h-11 flex justify-center items-center bg-zinc-100 hover:bg-gradient-to-b hover:from-ornage-400 hover:to-orange-500 hover:text-white cursor-pointer'>
+             <IoIosArrowBack />
+          </button>
+
+          <button className='text-2xl text-zinc-800 rounded-lg w-11 h-11 flex justify-center items-center bg-zinc-100 hover:bg-gradient-to-b hover:from-ornage-400 hover:to-orange-500 hover:text-white cursor-pointer'>
+             <IoIosArrowForward />
+          </button>
+
         </div>
-
-
 
       </div>
     </section>
