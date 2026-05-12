5183a2218ba51db3652805c9db00fca1972a2558
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-05-12 23:54:23 +0600

    error

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -102,6 +102,9 @@ const review = [
     para: 'I appreciate the selection of healthy foods and clean-label products.',
     image: Customer4,
   },
+
+
+  
   {
     id: 5,
     name: 'Natch Phongchal',
