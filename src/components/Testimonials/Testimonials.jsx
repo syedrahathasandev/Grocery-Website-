714fd593f6ae7afdbcaafcc54be745c165522a39
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-05-18 23:51:55 +0600

    update

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -110,5 +110,13 @@ const review = [
     para: 'FreshBasket offers high-quality groceries at reasonable prices.',
     image: Customer5,
   },
+  {
+    id: 6,
+    name: 'Natch Phongchal',
+    Profession: 'Nutritionist',
+    rating: 3,
+    para: 'FreshBasket offers high-quality groceries at reasonable prices.',
+    image: Customer5,
+  },
 
 ]
