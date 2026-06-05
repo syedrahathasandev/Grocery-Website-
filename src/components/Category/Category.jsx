6c01985e60bcfaf76a5198fe650a9f7ef4e20ebd
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-06-06 01:38:01 +0600

    update

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,28 +3,28 @@ import Heading from '../Heading/Heading'
 import FruitsCat from '../../assets/fruits-and-veggies.png'
 import DairyCat from '../../assets/dairy-and-eggs.png'
 import SeaFoodCat from '../../assets/meat-and-seafood.png'
-import Button from '../Button/Button'
+import { Link } from 'react-router-dom'
 
 
 const Category = () => {
 
-    const renderCards = category.map(card=>{
-      return(
-        // Card 
-        <div className='flex-1 basis-[300px]' key={card.id}>
-          {/* Card Image  */}
-          <div className='w-full min-h-[30vh] relative -mb-10'>
-            <img src={card.image} className='absolute bottom-0' />
-          </div>
-          {/* Card Content */}
-          <div className='bg-zinc-100 pt-17 p-8 rounded-xl'>
-            <h3 className='text-zinc-800 font-size-3xl font-bold'>{card.title}</h3>
-            <p className='text-zinc-600 mt-3 mb-9'>{card.description}</p>
-             <Button content="See All"/>
-          </div>
+  const renderCards = category.map(card => {
+    return (
+      // Card 
+      <div className='flex-1 basis-[300px]' key={card.id}>
+        {/* Card Image  */}
+        <div className='w-full min-h-[30vh] relative -mb-10'>
+          <img src={card.image} className='absolute bottom-0' />
         </div>
-      )
-    })
+        {/* Card Content */}
+        <div className='bg-zinc-100 pt-17 p-8 rounded-xl'>
+          <h3 className='text-zinc-800 font-size-3xl font-bold'>{card.title}</h3>
+          <p className='text-zinc-600 mt-3 mb-9'>{card.description}</p>
+          <Link to="/fruits" className='bg-gradient-to-b from-orange-400 to-orange-500 text-white px-8 py-3 rounded-lg md:text-lg text-md  hover:scale-105 hover:to-orange-600 transition-all duration-300 cursor-pointer'>See All</Link>
+        </div>
+      </div>
+    )
+  })
   return (
     <section>
       <div className='max-w-[1400px] mx-auto px-10 py-20'>
---
+++ src/components/Fruits/Fruits.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Fruits = () => {
+  return (
+    <div>
+      <h1>I am Fruits</h1>
+    </div>
+  )
+}
+
+export default Fruits
