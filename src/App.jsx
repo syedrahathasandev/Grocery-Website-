03fc5a831602433f4de88b329117aa30ffb4fe39
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-06-08 00:46:16 +0600

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Home from './components/Home/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Fruits from './components/Fruits/Fruits'
-import Dairy from './components/Dairy/Dairy'
+// import React from 'react'
+// import Home from './components/Home/Home'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import Fruits from './components/Fruits/Fruits'
+// import Dairy from './components/Dairy/Dairy'
 
-const App = () => {
+// const App = () => {
 
-  const router = createBrowserRouter([
-    {
-      path :'/',
-      element: <Home />,
-    },
-    {
-      path: '/fruits',
-      element: <Fruits />
-    },
-    {
-      path: '/dairy',
-      element: <Dairy />
-    }
-  ])
+//   const router = createBrowserRouter([
+//     {
+//       path :'/',
+//       element: <Home />,
+//     },
+//     {
+//       path: '/fruits',
+//       element: <Fruits />
+//     },
+//     {
+//       path: '/dairy',
+//       element: <Dairy />
+//     }
+//   ])
 
-  return <RouterProvider  router={router}/>
-}
+//   return <RouterProvider  router={router}/>
+// }
 
-export default App
+// export default App
