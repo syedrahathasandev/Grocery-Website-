2a5d1c08ac0788ba0d3d3e624ddbb08cc30dc410
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-06-10 01:13:32 +0600

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,7 +20,6 @@ const Home = () => {
       <Discount />
       <Process />
       <Testimonials />
-      <Footer />
     </div>
   )
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
 
 
 const Layout = () => {
   return (
     <div>
       <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
