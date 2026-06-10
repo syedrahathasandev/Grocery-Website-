e7654ec0dcaeed40822f00e7926acadb41876312
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-06-11 01:31:37 +0600

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
-// import Navbar from '../Navbar/Navbar'
-// import Hero from '../Hero/Hero'
-// import Category from '../Category/Category'
-// import Values from '../Values/Values'
-// import Products from '../Products/Products'
-// import Discount from '../Discount/Discount'
-// import Process from '../Process/Process'
-// import Testimonials from '../Testimonials/Testimonials'
-// import Footer from '../Footer/Footer'
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Hero from '../Hero/Hero'
+import Category from '../Category/Category'
+import Values from '../Values/Values'
+import Products from '../Products/Products'
+import Discount from '../Discount/Discount'
+import Process from '../Process/Process'
+import Testimonials from '../Testimonials/Testimonials'
+import Footer from '../Footer/Footer'
 
 const Home = () => {
   return (
