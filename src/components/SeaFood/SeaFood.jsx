1d031df48fe9952af0dbc3d49b9df4462ab139e6
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-06-27 17:21:52 +0600

    update

--- src/components/SeaFood/SeaFood.jsx
+++ src/components/SeaFood/SeaFood.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 
 const SeaFood = () => {
   return (
     <div>
-      <h1>Hi I'm SeaFood</h1>
+     <Banner />
     </div>
   )
 }
