09c6a9c46bf9b511a54a4a3430c88ba810a1bf6f
Syed Rahat Hasan <syedrahatdev@gmail.com>
2026-07-08 21:23:26 +0600

    update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-const Banner = () => {
-  return (
-    <div className='bg-zinc-40 h-[50vh]'>
-        <h2 className='text-5xl text-zinc-800 bg-white px-5 rounded-xl font-bold'>Banner</h2>
-    </div>
-  )
-}
+// const Banner = () => {
+//   return (
+//     <div className='bg-zinc-40 h-[50vh]'>
+//         <h2 className='text-5xl text-zinc-800 bg-white px-5 rounded-xl font-bold'>Banner</h2>
+//     </div>
+//   )
+// }
 
-export default Banner
+// export default Banner
